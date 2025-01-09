09ca418780bbe1a5d7cfaf3fdc65b310b818624d
Karthik Pattabiraman <karpat@meta.com>
2025-01-09 11:51:30 -0800

    Minor

--- graph.py
+++ graph.py
@@ -6,13 +6,13 @@ def plot_graph(sp, stats, statNames, xlabel, ylabel, verbose = False):
 
         t = np.arange(sp.start_rate, sp.end_rate + sp.increment_rate, sp.increment_rate)
         
-        print("StatNames ", statNames)
+        if verbose: print("StatNames ", statNames)
 
         # Get the list of statistics stored in the collector
         for statName in statNames:
                 # Extract each of the statistics
                 values = stats.extract(statName)
-                print(statName, values)
+                if verbose: print(statName, values)
 
                 # Plot the statistics as a line graph
                 plt.plot(t, values, label = statName)
